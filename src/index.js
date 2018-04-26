59b6e38b001910e7af81dc49652a06fc089db068
huakeubuntu <591509150@qq.com>
2018-04-26 17:05:55 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, Route, useRouterHistory} from 'react-router';
+import {createHistory} from 'history';
+import {Hello} from './app/hello';
+
+import './index.scss';
+
+// specify basename below if running in a subdirectory or set as "/" if app runs in root
+const appHistory = useRouterHistory(createHistory)({
+  basename: '/numberology/summary/'
+});
+
+ReactDOM.render(
+  <Router history={appHistory}>
+    <Route path="/" component={Hello}/>
+  </Router>,
+  document.getElementById('root')
+);
