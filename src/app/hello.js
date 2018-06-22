afb764869da26613a2a76a479fb9bae591cd2d36
huakeubuntu <591509150@qq.com>
2018-06-22 15:59:38 +0800

    https to httpp

--- src/app/hello.js
+++ src/app/hello.js
@@ -5,7 +5,7 @@ export class Hello extends Component {
   render() {
     console.log(cesuanArr);
     const cesuanLists = cesuanArr.map((cesuan, i) => {
-      return (<div className="cesuanItem" key={i}><a href={cesuan.url}><h3>{cesuan.name}</h3><p>@{cesuan.loc} - [{cesuan.from}]</p></a></div>);
+      return (<div className="cesuanItem" key={i}><a href={cesuan.url.replace('https', 'http')}><h3>{cesuan.name}</h3><p>@{cesuan.loc} - [{cesuan.from}]</p></a></div>);
     });
     return (
       <div>
